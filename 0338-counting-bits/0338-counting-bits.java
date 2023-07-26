4fb39b8b8d2fb59e94506b50a434a18d0d05bafa
Fiza Siddiqui <80641619+fiza1122@users.noreply.github.com>
2023-07-27 02:52:13 +0530

    Time: 1 ms (99.66%), Space: 46.4 MB (81.65%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.java
+++ 0338-counting-bits/0338-counting-bits.java
@@ -1,22 +1,27 @@
 class Solution {
-    public int[] countBits(int n) {
-        int ans[] = new int[n + 1];
+    public int[] countBits(int num) {
+    int[] f = new int[num + 1];
+    for (int i=1; i<=num; i++) f[i] = f[i >> 1] + (i & 1);
+    return f;
+}
+//     public int[] countBits(int n) {
+//         int ans[] = new int[n + 1];
 
-        for (int i = 0; i <= n; i++) {
-            ans[i] = countOnes(i);
-        }
+//         for (int i = 0; i <= n; i++) {
+//             ans[i] = countOnes(i);
+//         }
 
-        return ans;
-    }
+//         return ans;
+//     }
 
-    private int countOnes(int num) {
-        int count = 0;
-        while (num > 0) {
-            if (num % 2 == 1) {
-                count++;
-            }
-            num = num / 2;
-        }
-        return count;
-    }
+//     private int countOnes(int num) {
+//         int count = 0;
+//         while (num > 0) {
+//             if (num % 2 == 1) {
+//                 count++;
+//             }
+//             num = num / 2;
+//         }
+//         return count;
+//     }
 }
