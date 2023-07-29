6042cc8a1d80898bfb0f6a534c22324468916123
Fiza Siddiqui <80641619+fiza1122@users.noreply.github.com>
2023-07-29 22:36:21 +0530

    Time: 0 ms (100.00%), Space: 41.3 MB (14.50%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,5 +1,19 @@
 class Solution {
-    public double myPow(double x, int n) {
-       return Math.pow(x,n);  
+    // public double myPow(double x, int n) {
+    //    return Math.pow(x,n);  
+     public double myPow(double x, int n) {
+        if(n<0) return util(1/x, Math.abs((long)n));
+        else return util(x, n);    
+    }
+
+    private double util(double x, long n) {
+        if(n==0) return 1.0;
+        if(x==0) return 0.0;
+        
+        if(n%2==0) {
+            return util(x*x, n/2);
+        }else {
+            return x*util(x, n-1);
+        }
     }
 }
