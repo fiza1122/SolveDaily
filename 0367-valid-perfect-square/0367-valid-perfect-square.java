a95867b6aa84b69b8f03d063f07b9807523c9f23
Fiza Siddiqui <80641619+fiza1122@users.noreply.github.com>
2023-07-29 22:32:41 +0530

    Time: 0 ms (100.00%), Space: 39.2 MB (76.22%) - LeetHub

---
+++ 0367-valid-perfect-square/0367-valid-perfect-square.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean isPerfectSquare(int num) {
+        double n = Math.sqrt(num);
+        if( (int)n*n == num){
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+}
