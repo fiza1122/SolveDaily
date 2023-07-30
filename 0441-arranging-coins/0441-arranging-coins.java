bfe9b4bab34744d684a3e99c9687f6ef7121ae1c
Fiza Siddiqui <80641619+fiza1122@users.noreply.github.com>
2023-07-30 14:19:56 +0530

    Time: 1 ms (100.00%), Space: 39.7 MB (70.69%) - LeetHub

--- 0441-arranging-coins/0441-arranging-coins.java
+++ 0441-arranging-coins/0441-arranging-coins.java
@@ -1,12 +1,13 @@
 class Solution {
       int c=0,i=0;
     public int arrangeCoins(int n) {
-        i++;
-             if(n>0 && n>=i ){
-                 c++;
-                 arrangeCoins(n-i);
-             }
+//         i++;
+//              if(n>0 && n>=i ){
+//                  c++;
+//                  arrangeCoins(n-i);
+//              }
         
-        return c;
+//         return c;
+        return (int)(Math.sqrt(2 * (long)n + 0.25) - 0.5);
     }
 }
